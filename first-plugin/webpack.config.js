7a02491fddc0ea1ac219b6d19057ef24ba3cfdb3
BeAce <beaceshimin@gmail.com>
2018-01-18 11:55:45 +0800

    remove the hellow-wrold demo

--- first-plugin/webpack.config.js
+++ first-plugin/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const HelloWorldPlugin = require('./hello-world');
 const MyPlugin = require('./my-plugin');
 
 module.exports = {
